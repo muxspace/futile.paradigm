d133015ca8ceda4c5965773cb96bfb9f1a68190e
Brian Rowe <rowe@muxspace.com>
2010-06-11 19:31:09 +0000

    Forced declarations to be global to get tests to work

--- inst/unitTests/runit.functional.R
+++ inst/unitTests/runit.functional.R
@@ -1,27 +1,29 @@
+# For some reason these variables have to be declared globally, otherwise the
+# scoping within RUnit breaks the tests.
 absdiff <<- function(...) UseFunction('absdiff', ...)
 
 guard(absdiff.1, c(
   function(a,b) is.numeric(a) && is.numeric(b),
   function(a,b) a > b
 ))
-absdiff.1 <- function(a, b) a - b
+absdiff.1 <<- function(a, b) a - b
 
 guard(absdiff.2, c(
   function(a,b) is.numeric(a) && is.numeric(b),
   function(a,b) a < b
 ))
-absdiff.2 <- function(a, b) b - a
+absdiff.2 <<- function(a, b) b - a
 
 guard(absdiff.21, 
   function(a,b) ! is.numeric(a) || ! is.numeric(b)
 )
-absdiff.21 <- function(a, b) absdiff(as.double(a), as.double(b))
+absdiff.21 <<- function(a, b) absdiff(as.double(a), as.double(b))
 
 guard(absdiff.3, function(a) a > 5)
-absdiff.3 <- function(a) a * 2
+absdiff.3 <<- function(a) a * 2
 
 guard(absdiff.4, TRUE)
-absdiff.4 <- function(a) a + 1
+absdiff.4 <<- function(a) a + 1
 
 
 ## TESTS
